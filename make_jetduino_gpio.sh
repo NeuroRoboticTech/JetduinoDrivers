84942e5a094a5aec10ffd319e487bcbc8e74cbe6
dcofer <dcofer@NeuroRoboticTech.com>
2014-11-02 06:18:36 -0600

    Added initial jetduino_gpio test driver
    Added initial jetduino_gpio test driver and a compile script.

---
+++ make_jetduino_gpio.sh
@@ -0,0 +1,15 @@
+KERNEL_SRC="/usr/src/kernel"
+SPI_SRC="$KERNEL_SRC/drivers/spi"
+
+#echo $KERNEL_SRC
+#echo $SPI_SRC
+
+sudo cp -f ./jetduino_gpio.c $SPI_SRC/jetduino_gpio.c
+
+sudo make -C $KERNEL_SRC M=$SPI_SRC modules
+
+
+
+
+
+
