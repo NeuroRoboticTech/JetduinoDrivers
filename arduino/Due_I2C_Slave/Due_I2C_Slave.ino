61ec5a9cf8774e91de3cb40155ea6216a66817e1
David Cofer <dcofer@NeuroRoboticTech.com>
2015-04-27 08:06:16 -0500

    Added I2C tests for Arduino Due

---
+++ arduino/Due_I2C_Slave/Due_I2C_Slave.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+
+void setup()
+{
+  Serial.begin(57600);  // start serial for output
+  while(!Serial);
+  Serial.println("Starting setup");
+
+  Wire.begin(1);        // join i2c bus (address optional for master)
+  Wire.onRequest(requestEvent); // register event  
+  Wire.onReceive(receiveEvent); // register event
+}
+
+void loop()
+{
+  delay(100);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent()
+{
+  Wire.write("Slave 1"); // respond with message of 6 bytes
+
+  Serial.println("Slave 1 Sent");
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  String s = "";
+  while(Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    s += c;
+    //Serial.print(c);         // print the character
+  }
+  
+  Serial.print("Slave 1 Rx: ");
+  Serial.println(s);
+}
+
