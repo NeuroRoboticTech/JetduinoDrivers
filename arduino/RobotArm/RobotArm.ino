c8c95e33e8cf833d4b002797509a30c8b9c6e6c6
David Cofer <dcofer@NeuroRoboticTech.com>
2015-07-14 07:18:54 -0500

    Moved the code to set the system core clock into a method in DynamixelSerial

--- arduino/RobotArm/RobotArm.ino
+++ arduino/RobotArm/RobotArm.ino
@@ -50,20 +50,7 @@ void setServoPositions(int pos) {
 }
 
 void setup () {
- 
-  #define SYS_BOARD_PLLAR (CKGR_PLLAR_ONE | CKGR_PLLAR_MULA(39UL) | CKGR_PLLAR_PLLACOUNT(100UL) | CKGR_PLLAR_DIVA(3UL))
-  #define SYS_BOARD_MCKR ( PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK)
-          
-  // Set FWS according to SYS_BOARD_MCKR configuration 
-  EFC0->EEFC_FMR = EEFC_FMR_FWS(4); //4 waitstate flash access
-  EFC1->EEFC_FMR = EEFC_FMR_FWS(4);
-  
-  // Initialize PLLA to 114MHz 
-  PMC->CKGR_PLLAR = SYS_BOARD_PLLAR;
-  while (!(PMC->PMC_SR & PMC_SR_LOCKA)) {}
-  
-  PMC->PMC_MCKR = SYS_BOARD_MCKR;
-  while (!(PMC->PMC_SR & PMC_SR_MCKRDY)) {} 
+   SetSystemCoreClockFor1Mbaud();
  
 #ifdef ENABLE_DEBUG  
   Serial.begin(57600);
